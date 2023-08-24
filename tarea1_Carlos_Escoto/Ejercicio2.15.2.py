12cbe3d29dd7d75dce8ccf0cf2c1c8b2d61b69ac
CarlosEscoto2023 <143121945+CarlosEscoto2023@users.noreply.github.com>
2023-08-24 12:23:38 -0600

    Update Ejercicio2.15.2.py

--- tarea1_Carlos_Escoto/Ejercicio2.15.2.py
+++ tarea1_Carlos_Escoto/Ejercicio2.15.2.py
@@ -1,3 +1,5 @@
+"""Ejercicio 2: Escribe un programa que use input para pedirle al usuario
+su nombre y luego darle la bienvenida."""
 nombre = input("Escriba su nombre ")
 print(nombre)
 print(f"Hola, {nombre}")
